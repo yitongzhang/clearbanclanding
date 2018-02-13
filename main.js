7554a6c1488df0e287c14af25d366718d7d25da7
Yitong <zha.yitong@gmail.com>
2018-02-12 23:14:42 -0800

    fixed carousel

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  arrows:true,
+  autoplay:true,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
+				
